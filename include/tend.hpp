3960f59339ba64d06ec0e5a82585182ce3c69eb2
Mark Gross <mark.gross2001@gmail.com>
2020-03-01 22:46:15 -0800

    Add models for tend objects
    Add classes to represent records in each table of the tend database. The
    Entity class both represents a record in the entity table and serves as
    a base class for all other tend objects.

--- include/tend.hpp
+++ include/tend.hpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <optional>
 #include <ctime>
 
 namespace tend
@@ -10,9 +11,55 @@ namespace tend
 		private:
 			int id_;
 			time_t created_;
+			std::optional<time_t> updated_;
 
 		public:
 			int get_Id();
-			time_t get_Time();
+			time_t get_Created();
+			std::optional<time_t> get_Updated();
+	};
+
+	class Alias : public Entity
+	{
+		private:
+			int entity_id_;
+			std::string name_;
+
+		public:
+			int get_Entity_Id();
+			std::string get_Name();
+	};
+
+	class Relation : public Entity
+	{
+		private:
+			int entity_a_id_;
+			int entity_b_id_;
+
+		public:
+			int get_Entity_A_Id();
+			int get_Entity_B_Id();
+	};
+
+	class RelationSnippet : public Entity
+	{
+		private:
+			int relation_id_;
+			std::string data;
+
+		public:
+			int get_Relation_Id();
+			std::string get_Data();
+	};
+
+	class Snippet : public Entity
+	{
+		private:
+			int entity_id_;
+			std::string data;
+			
+		public:
+			int get_Entity_Id();
+			std::string get_Data();
 	};
 }
