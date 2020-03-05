b1813855c60d7519af4ac43b5b6dd2d388c7b389
Mark Gross <mark.gross2001@gmail.com>
2020-03-05 09:43:36 -0800

    Namespace style improvements
    Place main.cpp's code in the tend namespace and remove indentation in
    header file

--- include/tend.hpp
+++ include/tend.hpp
@@ -2,48 +2,49 @@
 #include <optional>
 #include <ctime>
 
-namespace tend
+namespace tend {
+
+std::string version();
+
+struct Entity
 {
-	std::string version();
+		int id;
+		std::time_t created;
+		std::optional<std::time_t> updated;
 
-	struct Entity
-	{
-			int id;
-			std::time_t created;
-			std::optional<std::time_t> updated;
+		Entity(const int id, const std::time_t created, std::optional<std::time_t> updated);
+};
 
-			Entity(const int id, const std::time_t created, std::optional<std::time_t> updated);
-	};
+struct Alias : public Entity
+{
+		Entity& entity;
+		std::string name;
 
-	struct Alias : public Entity
-	{
-			Entity& entity;
-			std::string name;
+		Alias(const int id, const std::time_t created, std::optional<std::time_t> updated, Entity& entity, std::string name);
+};
 
-			Alias(const int id, const std::time_t created, std::optional<std::time_t> updated, Entity& entity, std::string name);
-	};
+struct Relation : public Entity
+{
+		Entity& entity_a;
+		Entity& entity_b;
 
-	struct Relation : public Entity
-	{
-			Entity& entity_a;
-			Entity& entity_b;
+		Relation(const int id, const std::time_t created, std::optional<std::time_t> updated, Entity& entity_a, Entity& entity_b);
+};
 
-			Relation(const int id, const std::time_t created, std::optional<std::time_t> updated, Entity& entity_a, Entity& entity_b);
-	};
+struct RelationSnippet : public Entity
+{
+		Relation& relation;
+		std::string data;
 
-	struct RelationSnippet : public Entity
-	{
-			Relation& relation;
-			std::string data;
+		RelationSnippet(const int id, const std::time_t created, std::optional<std::time_t> updated, Relation& relation, std::string data);
+};
 
-			RelationSnippet(const int id, const std::time_t created, std::optional<std::time_t> updated, Relation& relation, std::string data);
-	};
+struct Snippet : public Entity
+{
+		Entity& entity;
+		std::string data;
 
-	struct Snippet : public Entity
-	{
-			Entity& entity;
-			std::string data;
+		Snippet(const int id, const std::time_t created, std::optional<std::time_t> updated, Entity& entity, std::string data);
+};
 
-			Snippet(const int id, const std::time_t created, std::optional<std::time_t> updated, Entity& entity, std::string data);
-	};
 } // namespace tend
--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,25 @@
 
 #include <tend.hpp>
 
-std::string tend::version()
+namespace tend {
+
+std::string version()
 {
 	return TEND_VERSION;
 }
 
-tend::Entity::Entity(const int id, const std::time_t created, std::optional<std::time_t> updated)
+Entity::Entity(const int id, const std::time_t created, std::optional<std::time_t> updated)
 	: id(id), created(created), updated(updated) {};
 
-tend::Alias::Alias(const int id, const std::time_t created, std::optional<std::time_t> updated, Entity& entity, std::string name)
+Alias::Alias(const int id, const std::time_t created, std::optional<std::time_t> updated, Entity& entity, std::string name)
 	: Entity(id, created, updated), entity(entity), name(name) {};
 
-tend::Relation::Relation(const int id, const std::time_t created, std::optional<std::time_t> updated, Entity& entity_a, Entity& entity_b)
+Relation::Relation(const int id, const std::time_t created, std::optional<std::time_t> updated, Entity& entity_a, Entity& entity_b)
 	: Entity(id, created, updated), entity_a(entity_a), entity_b(entity_b) {};
 
-tend::RelationSnippet::RelationSnippet(const int id, const std::time_t created, std::optional<std::time_t> updated, Relation& relation, std::string data)
+RelationSnippet::RelationSnippet(const int id, const std::time_t created, std::optional<std::time_t> updated, Relation& relation, std::string data)
 	: Entity(id, created, updated), relation(relation), data(data) {};
 
-tend::Snippet::Snippet(const int id, const std::time_t created, std::optional<std::time_t> updated, Entity& entity, std::string data)
+Snippet::Snippet(const int id, const std::time_t created, std::optional<std::time_t> updated, Entity& entity, std::string data)
 	: Entity(id, created, updated), entity(entity), data(data) {};
+}
